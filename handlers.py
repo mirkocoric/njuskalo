b625416d85119fd700b3b0818db97798fa501ec8
Mirko Coric <mirkocoric@gmail.com>
2017-06-27 17:55:00 +0200

    Improved name variables and docstrings

--- handlers.py
+++ handlers.py
@@ -1,11 +1,10 @@
 '''Handlers'''
 import njuskalo
 import tornado.web
-from tornado.httpclient import AsyncHTTPClient
 from tornado import gen
-from bs4 import BeautifulSoup
 
-class MainHandler(tornado.web.RequestHandler):
+
+class AdsHandler(tornado.web.RequestHandler):
     """Returns soup object for given url"""
 
     def initialize(self, homeurl):
@@ -13,39 +12,7 @@ class MainHandler(tornado.web.RequestHandler):
         self.homeurl = homeurl
 
     @gen.coroutine
-    def soup_from_url(self, url):
-        """Return soup from given url"""
-        http_client = AsyncHTTPClient()
-        response = yield http_client.fetch(url)
-        raise gen.Return(BeautifulSoup(response.body, 'lxml'))
-
-    def create_url(self, url, number):
-        '''Returns page url for given url and page number'''
-        string = "%20s%.2d" % (url, number+1)
-        return string
-
-    @gen.coroutine
-    def find_ads(self, page_num, url):
-        """Find ads from page_num pages from given url"""
-        links_articles = []
-        for number in xrange(page_num):
-            pageurl = self.create_url(url, number)
-            soup = yield self.soup_from_url(pageurl)
-            links_articles.append(njuskalo.find_all_ads(soup))
-        raise gen.Return(links_articles)
-
-    @gen.coroutine
-    def print_ads(self, homeurl):
-        """Print ads from homeurl"""
-        url = homeurl + '?page='
-        soup = yield self.soup_from_url(url)
-        page_num = njuskalo.find_last_page_number(soup)
-        links_articles = yield self.find_ads(page_num, url)
-        raise gen.Return(njuskalo.return_ads(links_articles))
-
-    @gen.coroutine
     def get(self):
-        """Main request handler"""
-        ads = yield self.print_ads(self.homeurl)
+        """Returns ads from url and prints them in a web browser"""
+        ads = yield njuskalo.return_ads_from_url(self.homeurl)
         self.write(''.join(ads))
-
--- njuskalo.py
+++ njuskalo.py
@@ -3,15 +3,21 @@ from __future__ import print_function
 import argparse
 from collections import namedtuple
 import re
+import handlers
 import tornado.ioloop
 import tornado.web
-import handlers
+from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
+from bs4 import BeautifulSoup
 
-class Adtuple(namedtuple('adtuple', 'naslov cijena')):
+
+class AdTuple(namedtuple('adtuple', 'naslov cijena')):
     '''Stores naslov and cijena for each ad in a single element'''
     def __str__(self):
-        return ''.join(['Naslov: ', self.naslov, ' ', 'Cijena: ', ' '.join
-                        (self.cijena), '\r\n']).encode('utf-8')
+        return ''.join(['Naslov: ', self.naslov, ' ',
+                        'Cijena: ', ' '.join(self.cijena),
+                        '\r\n']).encode('utf-8')
+
 
 def is_int(name):
     """Returns True if it is possible to convert string to integer"""
@@ -21,64 +27,110 @@ def is_int(name):
     except ValueError:
         return False
 
+
 def make_price(pricetag_list):
     """Returns list of prices"""
     return [re.sub(r'~', '', pricetag.text) for pricetag in pricetag_list]
 
+
 def find_atags(soup):
     """Finds all a tags in soup"""
-    yield (atag for article in soup.find_all('article')
-           for h3tag in article.find_all(name='h3')
-           for atag in h3tag.find_all(name='a'))
+    return (atag for article in soup.find_all('article')
+            for h3tag in article.find_all(name='h3')
+            for atag in h3tag.find_all(name='a'))
+
 
 def find_all_ads(soup):
-    """Finds all articles and returns array of ad namedtuples which include title and price"""
-    genatags = find_atags(soup)
-    yield (Adtuple(atag.text, make_price(atag.parent.parent.find_all(class_='price')))
-           for atags in genatags
-           for atag in atags)
+    """Finds all articles and returns array of AdTuples"""
+    atags = find_atags(soup)
+    return (AdTuple(atag.text, make_price(atag.parent.parent.find_all
+                                          (class_='price')))
+            for atag in atags)
+
 
 def find_paging_links(soup):
     """Finds page link number"""
-    yield (int(num) for button in soup.find_all('button')
-           for num in button.text.split("-")
-           if 'data-page' in button.attrs
-           if is_int(num))
+    return (int(num) for button in soup.find_all('button')
+            for num in button.text.split("-")
+            if 'data-page' in button.attrs
+            if is_int(num))
+
 
 def find_last_page_number(soup):
     """Calculates number of pages with articles"""
     numbers = find_paging_links(soup)
-    return map(max, numbers)[0]
+    return max(numbers)
 
-def return_ads(listgenads):
+
+def ads_to_strings(ads):
     """Returns all ads as a list of strings"""
-    return (map(str,
-                [ad for genads in listgenads
-                 for ads in genads
-                 for ad in ads]))
+    return map(str, ads)
+
 
 def parse_args():
-    """Parses program arguments (url with ads)"""
-    description_string = 'Finding items from njuskalo and prints title and price'
+    """Parses program arguments
+    Returns home url and port"""
+    description_string = 'Finding ads and prints title and price'
     parser = argparse.ArgumentParser(description=description_string)
     parser.add_argument('home',
                         help='home url')
     parser.add_argument('--port',
                         help='port',
                         default=8500)
-    return parser.parse_args().home, parser.parse_args().port
+    return parser.parse_args()
 
-def start_ad_service():
+
+def create_url(url, number):
+    '''Returns page url for given url and page number'''
+    string = "%20s?page=%.2f" % (url, number+1)
+    return string
+
+
+@gen.coroutine
+def find_ads(page_num, url):
+    '''Find ads from page_num pages from given url
+    Returns a list of pages where each page is a list of AdTuple objects'''
+    links_articles = []
+    for number in xrange(page_num):
+        pageurl = create_url(url, number)
+        soup = yield soup_from_url(pageurl)
+        ads = find_all_ads(soup)
+        links_articles += ads
+    raise gen.Return(links_articles)
+
+
+@gen.coroutine
+def return_ads_from_url(homeurl):
+    """Print ads from homeurl"""
+    soup = yield soup_from_url(homeurl)
+    page_num = find_last_page_number(soup)
+    links_articles = yield find_ads(page_num, homeurl)
+    raise gen.Return(ads_to_strings(links_articles))
+
+
+@gen.coroutine
+def soup_from_url(url):
+    """Return soup from given url"""
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(url)
+    raise gen.Return(BeautifulSoup(response.body, 'lxml'))
+
+
+def start_ad_service(homeurl=None, port=None):
     """Launches the Tornado service for Ads"""
-    homeurl, port = parse_args()
+    if not homeurl or not port:
+        args = parse_args()
+        homeurl = args.home
+        port = args.port
     app = make_app(homeurl)
     app.listen(port)
     tornado.ioloop.IOLoop.current().start()
 
+
 def make_app(homeurl):
     """Creates web application"""
     return tornado.web.Application([
-        (r"/", handlers.MainHandler, dict(homeurl=homeurl))
+        (r"/", handlers.AdsHandler, dict(homeurl=homeurl))
     ])
 
 if __name__ == "__main__":

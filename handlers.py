89ee42951225efa0ecd9d49e67b127d636091dce
Mirko Coric <mirkocoric@gmail.com>
2017-06-29 16:44:57 +0200

    Added module FetchAds

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 '''Handlers'''
-import FetchAds
+import fetch_ads
 import tornado.web
 from tornado import gen
 
@@ -14,5 +14,5 @@ class AdsHandler(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
         """Returns ads from url and prints them in a web browser"""
-        ads = yield FetchAds.ads_from_url(self.homeurl)
+        ads = yield fetch_ads.ads_from_url(self.homeurl)
         self.write(''.join(ads))
